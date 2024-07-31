4f6a8208ad59b1e1b6b115382afe70bf4f8df54f
RodrigoVieira06 <rodrigovlima06@gmail.com>
2024-07-31 18:58:07 -0300

    feat: developed the header component

--- src/app/pages/manufacturers/manufacturers.component.ts
+++ src/app/pages/manufacturers/manufacturers.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './manufacturers.component.scss'
 })
 export class ManufacturersComponent {
-
+  public title: string = 'Fabricantes';
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
-
+  public title: string = 'Produtos';
 }
