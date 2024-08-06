9b308fb1ffabefa9462bd5e9357ea0839bee3f5e
RodrigoVieira06 <rodrigovlima06@gmail.com>
2024-08-06 02:09:51 -0300

    improvements: responsiveness adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MatDrawerMode } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent { }
+export class AppComponent {
+  public sidenavMode: MatDrawerMode;
+  public sidenavOpened: boolean;
+
+  constructor() {
+    if (typeof window !== "undefined" && window.innerWidth > 1050) {
+      this.sidenavMode = 'side';
+      this.sidenavOpened = true;
+    } else {
+      this.sidenavMode = 'over';
+      this.sidenavOpened = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if (event.target.innerWidth < 1050) {
+      this.sidenavMode = 'over';
+      this.sidenavOpened = false;
+    } else {
+      this.sidenavMode = 'side';
+      this.sidenavOpened = true;
+    }
+  }
+}
