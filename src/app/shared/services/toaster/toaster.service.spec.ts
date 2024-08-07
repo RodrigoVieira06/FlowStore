1de4394ade8938e4419a546dae52aa03e325e30f
Rodrigo Vieira Lima <rodrigovlima06@gmail.com>
2024-08-07 04:04:30 -0300

    test: developed toaster service unit tests

--- src/app/shared/services/toaster/toaster.service.spec.ts
+++ src/app/shared/services/toaster/toaster.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ToasterService } from './toaster.service';
+import { Subject } from 'rxjs';
+import { ToastMessage } from '../../models/toast-message';
 
 describe('ToasterService', () => {
   let service: ToasterService;
+  let toastSubject: Subject<ToastMessage>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +15,20 @@ describe('ToasterService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should emit a toast message', (done) => {
+    const testMessage = 'Test message';
+    const testType: 'success' | 'error' | 'info' | 'warning' = 'success';
+
+    service.toastState.subscribe({
+      next: (message: ToastMessage) => {
+        expect(message.message).toBe(testMessage);
+        expect(message.type).toBe(testType);
+        done();
+      }
+    });
+
+    service.showToast(testMessage, testType);
   });
 });
