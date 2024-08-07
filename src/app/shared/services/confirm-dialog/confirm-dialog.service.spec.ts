7af0afc92167da65f9ce678f4b2f2aa9ea828ce6
Rodrigo Vieira Lima <rodrigovlima06@gmail.com>
2024-08-07 04:04:19 -0300

    test: developed confirm dialog service unit tests

--- src/app/shared/services/confirm-dialog/confirm-dialog.service.spec.ts
+++ src/app/shared/services/confirm-dialog/confirm-dialog.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ConfirmDialogService } from './confirm-dialog.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 describe('ConfirmDialogService', () => {
   let service: ConfirmDialogService;
+  let dialog: MatDialog;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ConfirmDialogService);
+    dialog = TestBed.inject(MatDialog);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return a MatDialogRef instance', () => {
+    const result = service.open('Title', 'Body');
+    expect(result).toBeInstanceOf(MatDialogRef);
   });
 });
